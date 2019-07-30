c05d581e281ea5f57af51162b1747c65bcd04cee
Bogdan Tsubeks <tsubekx@gmail.com>
2019-07-30 21:58:24 +0300

    test

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,39 +1,50 @@
 import React from 'react';
-import {createGlobalStyle} from 'styled-components';
+import {ThemeProvider, createGlobalStyle} from 'styled-components';
 import {normalize} from 'styled-normalize';
+import {Link, Flex, Box, Heading, Button, Image} from 'rebass';
 
+import themes from '../themes';
 import logo from '../assets/logo.svg';
 
 const GlobalStyle = createGlobalStyle`
 	${normalize}
-	body {
-		background: black;
-	}
 
-	p {
-		margin: 0;
+	html, body {
+		font-size: 10px;
+		background: black;
 	}
 `;
 
+const Header = () => (
+  <Flex px={2} as="header" bg="tomato">
+    <Link p={2} href="https://tsubeks.xyz">
+      <Image src={logo} alt="logo" width="64" bg="red" borderRadius={8} />
+    </Link>
+    <Box mx="auto" />
+    <Link p={2} href="https://tsubeks.xyz">
+      <Image src={logo} alt="logo" width="64" bg="red" borderRadius={8} />
+    </Link>
+  </Flex>
+);
+
 const Root = (
-  <>
-    <GlobalStyle />
-    <header style={{background: 'red'}}>Header</header>
-    <main style={{background: 'blue'}}>
-      Content
-      <img src={logo} alt="logo" width="256" />
-      <p>
-        Edit <code>src/Root.js</code> and save to reload.
-      </p>
-    </main>
-    <footer style={{background: 'yellow'}}>
-      $ created by&nbsp;
-      <a href="https://tsubeks.xyz" target="_blank" rel="noopener noreferrer">
-        me
-      </a>
-      &nbsp;$
-    </footer>
-  </>
+  <ThemeProvider theme={themes['light']}>
+    <>
+      <GlobalStyle />
+      <Header />
+      <main style={{background: 'blue'}}>
+        <Heading color="magenta">Content</Heading>
+        <Button>change theme</Button>
+      </main>
+      <footer style={{background: 'yellow'}}>
+        $ created by&nbsp;
+        <a href="https://tsubeks.xyz" target="_blank" rel="noopener noreferrer">
+          me
+        </a>
+        &nbsp;$
+      </footer>
+    </>
+  </ThemeProvider>
 );
 
 export default Root;
