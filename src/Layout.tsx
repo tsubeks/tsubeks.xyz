feb535ef91f88c9962d593c6518cb6be9e8d801a
Bogdan Tsubeks <tsubekx@gmail.com>
2019-08-09 23:11:14 +0300

    add theme support

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,31 +1,90 @@
+/** @jsx jsx */
 import React from "react";
+import { jsx, Styled, useColorMode } from "theme-ui";
 import { Link } from "react-router-dom";
+import {
+  FaGithub as Github,
+  FaBookDead as Book,
+  FaBullseye as Bull
+} from "react-icons/fa";
 
 import logo from "./assets/logo.svg";
 
-const Header = () => (
-  <header>
+const modes = ["light", "black", "dark", "deep", "hack", "pink"];
+
+const ThemeButton = (props: any) => {
+  const colorMode = useColorMode();
+
+  const cycleMode = (e: any, [mode, setMode]: any) => {
+    const i = modes.indexOf(mode);
+    const n = (i + 1) % modes.length;
+    setMode(modes[n]);
+  };
+
+  return (
+    <button
+      {...props}
+      title="ThemeButton"
+      onClick={e => cycleMode(e, colorMode)}
+      sx={{
+        border: "none",
+        bg: "primary",
+        color: "text"
+      }}
+    >
+      <Bull size={48} />
+    </button>
+  );
+};
+
+const Logo = () => <div sx={{ width: 48, height: 48, bg: "red" }}></div>;
+
+const Header = ({ children }: { children?: React.ReactNode }) => (
+  <header sx={{ display: "flex", px: 2, width: "100%" }}>
     <Link to="/">
-      <img width="24" alt="logo" src={logo} />
+      <Logo />
     </Link>
-    <Link to="/about">About</Link>
-    <Link to="/book">Book</Link>
+    <div sx={{ width: "100%" }} />
+    <nav>
+      {children}
+      <Link to="/about">About</Link>
+      <Link to="/book">Book</Link>
+    </nav>
+    <ThemeButton />
   </header>
 );
 
+const Main = ({ children }: { children?: React.ReactNode }) => {
+  return <main sx={{ bg: "secondary" }}>{children}</main>;
+};
+
+const Footer = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <footer sx={{ bg: "primary" }}>
+      $ created by&nbsp;
+      <a href="https://tsubeks.xyz" target="_blank" rel="noopener noreferrer">
+        me
+      </a>
+      &nbsp;$
+      {children}
+    </footer>
+  );
+};
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        bg: "primary"
+      }}
+    >
       <Header />
-      <main style={{ background: "blue" }}>{children}</main>
-      <footer style={{ background: "yellow" }}>
-        $ created by&nbsp;
-        <a href="https://tsubeks.xyz" target="_blank" rel="noopener noreferrer">
-          me
-        </a>
-        &nbsp;$
-      </footer>
-    </>
+      <Main>{children}</Main>
+      <Footer />
+    </div>
   );
 };
 
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ThemeProvider } from "theme-ui";
 import { Global } from "@emotion/core";
 
 import Layout from "./Layout";
+import theme from "./theme";
 
 const meta = {
   title: "Bogdan Tsubeks",
@@ -26,32 +28,34 @@ const Book = () => {
 const Root = () => {
   return (
     <Router>
-      <Global
-        styles={{
-          "*": {
-            boxSizing: "border-box"
-          },
-          body: {
-            margin: 0
-          }
-        }}
-      />
-      <Helmet>
-        <meta name="twitter:site" content="@purenoxyz" />
-        <meta name="og:image" content="" />
-        <title>{meta.title}</title>
-        <meta name="og:title" content={meta.title} />
-        <meta name="og:description" content={meta.description} />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:creator" content={meta.creator} />
-        <meta name="twitter:card" content="summary" />
-      </Helmet>
-      <Layout>
-        <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
-        <Route path="/book/" component={Book} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Global
+          styles={{
+            "*": {
+              boxSizing: "border-box"
+            },
+            body: {
+              margin: 0
+            }
+          }}
+        />
+        <Helmet>
+          <meta name="twitter:site" content="@purenoxyz" />
+          <meta name="og:image" content="" />
+          <title>{meta.title}</title>
+          <meta name="og:title" content={meta.title} />
+          <meta name="og:description" content={meta.description} />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:creator" content={meta.creator} />
+          <meta name="twitter:card" content="summary" />
+        </Helmet>
+        <Layout>
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/book/" component={Book} />
+        </Layout>
+      </ThemeProvider>
     </Router>
   );
 };
