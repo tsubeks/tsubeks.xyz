4c2704515a3511e842eeaaa220acf1dd454bba81
Bogdan Tsubeks <tsubekx@gmail.com>
2019-08-19 08:18:07 +0300

    Updates

---
+++ precache-manifest.e41633b030bca68c75a6f7cd214ffa66.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9815acdb238c2546cbd124a0177f280",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d75d5aeec2846c68ad59",
+    "url": "/static/js/2.8e077910.chunk.js"
+  },
+  {
+    "revision": "61baf1ec09742c86cd14",
+    "url": "/static/js/main.ac4a3062.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.e41633b030bca68c75a6f7cd214ffa66.js"
 );
 
 self.addEventListener('message', (event) => {
